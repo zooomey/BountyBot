adf7c6e573974f65657b1bee60324bf76f2e8b6d
Claire Toomey <74575438+zooomey@users.noreply.github.com>
2022-04-07 19:24:40 -0400

    commit

--- bot.py
+++ bot.py
@@ -11,6 +11,28 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD= os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
+
+#class bounty:
+#initialize time as 0
+#initialize points as 100
+
+#class hunter:
+#hunter only exists if they collect bounty
+#initialize hunter and assign bounty score
+#remove bountied individual from board
+
+#class passday:
+#if time = 30 -> remove from bounty board
+#users bounty increases by + 50
+#increase all user time by 1
+
+#class board:
+#display top 10 highest bounties on users
+#if less than 10 users exist, only display what's there
+
+#class leaderboard:
+#display top 10 bounty hunters
+#
    
 class commands(discord.Client):
     async def on_message(self, message):
